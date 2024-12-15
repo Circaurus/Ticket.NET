83d29ef641c3715f075568c8287555719956d8dd
unknown <shawnbasnett@gmail.com>
2024-12-15 00:58:59 -0500

    Initial Commit with basic HTTP Client

---
+++ TicketNET/TicketNET/Models/Events.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketNET.Models
+{
+    public class Events : BaseResponse
+    {
+        public Embedded _embedded {  get; set; }
+
+        public class Embedded
+        {
+            public Event[] events { get; set; }
+        }
+
+        public class Event
+        {
+            public string name { get; set; }
+            public string description { get; set; }
+            public string id { get; set; }
+            public string url { get; set; }
+            public Dates[] dates { get; set; }
+
+        }
+
+        public class Dates
+        {
+            public EventDateInfo[] start { get; set; }
+        }
+
+        public class EventDateInfo
+        {
+            public string localDate { get; set; }
+        }
+    }
+}
---
+++ TicketNET/TicketNET/TMClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TicketNET.Models;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TicketNET
+{
+    public class TMClient
+    {
+        private const string Endpoint = "https://app.ticketmaster.com/discovery/v2/";
+        private const string eventRoute = "events.json?";
+        private const string classificationName = "classificationName=music&";
+
+        public string Country {  get; private set; }
+
+        public TMClient(string country)
+        {
+            Country = country;
+        }
+
+
+        public async Task<Events> GetEventsAsync(string apiKey)
+        {
+            var result = await GetEventRequestAsync<Events>(classificationName, apiKey);
+
+            return result;
+        }
+
+
+
+        private async Task<T> GetEventRequestAsync<T>(string request, string apiKey)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Endpoint);
+
+                    var result = await client.GetAsync(eventRoute + request + apiKey);
+                    var contents = await result.Content.ReadAsStringAsync();
+                    var modelObject = JsonConvert.DeserializeObject<T>(contents);
+
+                    return modelObject;
+                }
+            }
+            catch (Exception ex) 
+            {
+                return default(T);
+            }
+        }
+
+
+    }
+}
